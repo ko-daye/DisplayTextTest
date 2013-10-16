5a9978f92ced1090565fbb59b14087ad74d074a8
ko-daye <kodaye@gmail.com>
2013-10-17 07:30:45 +0900

    first commit

---
+++ src/com/example/displaytext/test/MainActivityTest.java
@@ -0,0 +1,53 @@
+package com.example.displaytext.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.displaytext.MainActivity;
+import com.example.displaytext.R;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	
+	private MainActivity mActivity;
+	private EditText mInputLine;
+	private Button mAddButton;
+	private Button mClearButton;
+	private TextView mDisplayText;
+	
+	private String mAddButtonString;
+	private String mClearButtonString;
+
+	public MainActivityTest() {
+		super(MainActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		mActivity = getActivity();
+		
+		mInputLine = (EditText)mActivity.findViewById(R.id.editBox);
+		mAddButton = (Button)mActivity.findViewById(R.id.addButton);
+		mClearButton = (Button)mActivity.findViewById(R.id.deleteButton);
+		mDisplayText = (TextView)mActivity.findViewById(R.id.textBox);
+		
+		mAddButtonString = mActivity.getString(R.id.addButton);
+		mClearButtonString = mActivity.getString(R.id.deleteButton);
+	}
+	
+	public void testPrecondition() {
+		
+		assertEquals(mInputLine.getText().toString(), "");
+		
+		assertEquals(mAddButton.getText().toString(), mAddButtonString);
+		
+		assertEquals(mClearButton.getText().toString(), mClearButtonString);
+		
+		assertFalse(mClearButton.isEnabled());
+		
+		assertEquals(mDisplayText.getText().toString(), "");
+	}
+	
+}
